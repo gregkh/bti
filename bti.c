35f9afd2df1cc9b633ec9eee01a3292dc7eb5ccd
Amir Mohammad Saied <amirsaied@gmail.com>
2009-05-27 22:32:06 +0430

    Updating AUTHORs and copyright section

--- bti.c
+++ bti.c
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2008 Greg Kroah-Hartman <greg@kroah.com>
  * Copyright (C) 2009 Bart Trojanowski <bart@jukie.net>
+ * Copyright (C) 2009 Amir Mohammad Saied <amirsaied@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
