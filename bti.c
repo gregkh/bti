84f1c80f93dd6f80fc11b3b0431864f8adc391a2
Amir Mohammad Saied <amirsaied@gmail.com>
2010-09-02 18:12:07 +0430

    Update my copyright date.

--- bti.c
+++ bti.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2008-2010 Greg Kroah-Hartman <greg@kroah.com>
  * Copyright (C) 2009 Bart Trojanowski <bart@jukie.net>
- * Copyright (C) 2009 Amir Mohammad Saied <amirsaied@gmail.com>
+ * Copyright (C) 2009-2010 Amir Mohammad Saied <amirsaied@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
